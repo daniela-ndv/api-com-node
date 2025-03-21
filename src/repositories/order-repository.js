fb8fbf6212aca6cda80e26ffa52da2cb99729a33
Daniela Dalla Vecchia <114515986+daniela-ndv@users.noreply.github.com>
2025-03-21 17:13:09 -0300

    utilizando populate

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async(data) => {
-    var res = await Order.find({});
+    var res = await Order
+        .find({}, 'number status customer items')
+        .populate('customer')
+        .populate('items.product');
     return res;
 }
 
