844814518e92b34bd94534e8e45f3d73a7a58448
Daniela Dalla Vecchia <114515986+daniela-ndv@users.noreply.github.com>
2025-03-27 10:11:38 -0300

    Autenticação e autorização

--- src/repositories/customer-repository.js
+++ src/repositories/customer-repository.js
@@ -1,12 +1,26 @@
 const mongoose = require('mongoose');
 const Customer = mongoose.model('Customer');
 
-exports.get = async(data) => {
-    var res = await Order.find({});
+exports.get = async() => {
+    const res = await Customer.find({}, 'name email roles')
     return res;
 }
 
 exports.create = async(data) => {
     var customer = new Customer(data);
     await customer.save();
+}
+
+exports.authenticate = async(data) => {
+    var res = await Customer.findOne({
+        email: data.email,
+        password: data.password,
+        roles: data.roles
+    });
+    return res;
+}
+
+exports.getById = async(id) => {
+    var res = await Customer.findById(id);
+    return res;
 }
